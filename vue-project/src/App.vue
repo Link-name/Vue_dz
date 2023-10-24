55004621ab17b980780f4c45a29d67b37e1444d7
Link <stereo_house01@mail.ru>
2023-10-24 19:29:17 +0300

    ///

---
+++ vue-project/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    
+<!--     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <CalcComponet></CalcComponet> -->
+  </div>
+</template>
+
+<script>
+
+import CalcComponet from './components/CalcComponet'
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    /* CalcComponet,
+    HelloWorld */
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-project/src/components/CalcComponet.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="calc" >
+        <input v-model="result" class="input" type="text">
+        <button @click="addButton(num)" v-for="num in nums" :key="num.id">
+            {{ num }}
+        </button>
+        <button @click="addButton(tab)" v-for="tab in tabs" :key="tab.id">
+            {{ tab }}
+        </button>
+        <button @click="caclResalt">=</button>
+        
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'CalcComponent',
+   
+    data() {
+      return {
+        result:0,
+        nums:[1,2,3,4,5,6,7,8,9,0],
+        tabs:['-','+','/','*']
+      }
+    },
+    methods: {
+        addButton(har){
+            this.result = this.result.toString()
+            this.result+=har
+        },
+        caclResalt(){
+            this.result = eval(this.result)
+        }
+
+
+    },
+  }
+  </script>
+  
+ 
+  <style lang="css" scoped>
+    .calc {
+        display: grid;
+        grid-template-columns: repeat(3,150px);
+        justify-content: center;
+        padding-top: 40px;
+    }
+    .input{
+        text-align: right;
+        height: 20px;
+        margin-bottom: 10px;
+        grid-column: 1/-1;
+    }
+  </style>
+  
